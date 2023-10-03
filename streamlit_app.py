d6600116be1eb8435aaa1a61ccb8c090103296d1
pmercadogithub <79717086+pmercadogithub@users.noreply.github.com>
2023-10-03 15:21:26 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,6 +45,7 @@ streamlit.dataframe(my_data_rows)
 
 #
 add_my_fruit = streamlit.text_input('What fruit would you like information about to add?','jackfruit')
-streamlit.write('The user entered ', add_my_fruit)
+streamlit.write('Thanks for adding ', add_my_fruit)
 
-
+#
+my_cur.execute("insert into FRUIT_LOAD_LIST values ('from streamlit')")
