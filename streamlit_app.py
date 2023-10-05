d4fd4a0eec1c99b9fb692e13b617571b26d57d7c
pmercadogithub <79717086+pmercadogithub@users.noreply.github.com>
2023-10-04 23:14:55 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,14 +30,14 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-
+###############################################################################################
 #New Section to dispaly fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 def get_fruityvice_data(this_fruit_choice):
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
         fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
         return fruityvice_normalized
-
+###############################################################################################
 
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
@@ -64,16 +64,17 @@ if streamlit.button('Get Fruit Load List2'):
 	my_data_rows= get_fruit_load_list()
 	my_cnx.close()
 	streamlit.dataframe(my_data_rows)
-
+	
+###############################################################################################
 #Allow user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
 	with my_cnx.cursor() as my_cur:
 		my_cur.execute("insert into fruit_load_list values ('%s')" %fruit_choice_2)
 		return 'Thanks for adding ' + new_fruit
-
+###############################################################################################
 
 #add_my_fruit = streamlit.text_input('What fruit would you like information about to add3?')
 if streamlit.button('Add a Fruit to the List'):
-    back_from_funtion = insert_row_snowflake(add_my_fruit)
-    streamlit.text(back_from_funtion)
+	back_from_funtion = insert_row_snowflake(add_my_fruit)
+	streamlit.text(back_from_funtion)
 										
